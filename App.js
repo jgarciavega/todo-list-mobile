01290dfecb9090d695b7bce4c6a776a48224f6d4
Angel Ojeda <angel.ojeda.castro@gmail.com>
2023-09-16 00:14:32 -0700

    Initial Commit

--- App.js
+++ App.js
@@ -1,20 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box, Container } from "native-base";
+import CustomInput from "./CustomInput";
+import Login from "./Components/Login";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Box flex={1} bg="#fff" alignItems="center" justifyContent="center">
+        <Container>
+          <Login />
+          {/* Este componente es solo un ejemplo */}
+          <CustomInput text="Usuario" />
+        </Container>
+      </Box>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
